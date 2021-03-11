30c9ceea2c3d41da80e4166eddb3ae026607c608
Nambinina13 <hery.andrianantenaina@ens.uvsq.fr>
2021-03-11 13:46:09 +0100

    Creating structur for sensor

--- capteur.h
+++ capteur.h
@@ -4,33 +4,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <proc/readproc.h>
-struct state
+
+#define PROC_FLAGS (PROC_FILLMEM | PROC_FILLCOM | PROC_FILLENV | PROC_FILLUSR |PROC_FILLGRP | PROC_FILLSTAT | PROC_FILLSTATUS | PROC_EDITCMDLCVT)
+
+
+typedef struct proc_info_s
 {
-    char name[1000];
-    char statue;
-    int parentID;
-    long unsigned int utime;
-    long int cutime;
-    long unsigned int stime;
-    long int cstime;
-    long int priority;
-    long int  nice;
-    long long unsigned int starttime;
-    long unsigned int vsize; // virtual memory size in bytes
-    long unsigned int rss; //Resident  Set  Size in bytes
-    long unsigned int pTime;
-    float cpu_total_time;
-    };
+	size_t n;
+	proc_t **info;
+}proc_info_t;
 
-typedef struct machine_info
-{
-  size_t size_memorie;
-  char *name;
-
-}machine_info_t;
-
-long unsigned int get_uptime();
-
-void process_value(long pid);
+proc_info_t *capteur();
 
 #endif
