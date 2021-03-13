a0763c5fd15feff359ef13312ae906631b9e8be5
Sulaiman_Mohammad <sulaiman.ayman.mohammad@gmail.com>
2021-03-13 11:11:18 +0100

    edited

--- htophandy.c
+++ htophandy.c
@@ -1,112 +1,124 @@
 #include "readprocess.h"
+
+//macros to clear the output and refrech the output
 #define update() printf("\033[H\033[J")
 #define gotoxy(x, y) printf("\033[%d;%dH", x, y)
 
-
+//the main function to calculated the data of the process
 void process_value(int pid)
- {
+{
+  //object for the cpu pid data
   struct cpu_info* cpud;
   cpud = malloc(sizeof *cpud);
 
+  //object for the process data
   struct state* proc;
   proc = malloc(sizeof *proc);
 
-  read_statFile(proc,pid);
+   //object for the memory data
+  struct memorydata* memo;
+  memo = malloc(sizeof *memo);
 
-  get_process_cpu_usage(cpud,proc, pid);
+  read_statFile(proc, pid);// read the process data
 
-  comput_process_time(proc, pid);
+  get_process_cpu_usage(cpud, proc, pid);//calculated the process cpu usage
 
-  proc->rss=(proc->rss * getpagesize())/1000;//in bytes to kb
+  comput_process_time(proc, pid);//since when the process is runing
 
-  get_total_mem(cpud);
+  get_memory_usage(proc,memo);
 
-  proc->mem = ((float) proc->rss/cpud->total_memkb)*100;
+  // find_usrname(pid);
 
- // find_usrname(pid);
-
-  get_sharedmemory_size(proc,pid);
+  get_sharedmemory_size(memo,pid); // shared memory for the process
 
   proc->vsize=((int)(proc->vsize)/(int)1000000);//vsize in byte
 
   printf("%8d",pid);
- // printf("%12s\n",find_usrname(pid));
+  printf("%12s",find_usrname(pid));
   printf("%7ld",proc->priority);
   printf("%8ld",proc->nice);
   printf("%10dM",proc->vsize);
   printf("%9ldM",(proc->rss)/1000);
-  printf("%8luM",(proc->shared_m_size)/1000000);
+  printf("%8luM",(memo->shared_m_size)/1000000);
   printf("%8c",proc->statue);
-  printf("%11.2f",proc->cpu_total_time );
-  printf("%9.2f",proc->mem);
+  printf("%11.2f",cpud->cpu_total_time );
+  printf("%9.2f",memo->mem);
   printf("%9lu",proc->process_time);
   printf("%12s\n",proc->name);
-  char buf[100];
-  write(1,buf,proc->name);
+  //char buf[100];
+  //write(1,buf,proc->name);
   fflush(stdout);
 
- free(cpud);
- free(proc);
+  //free the object and releae the memory
+  free(cpud);
+  free(proc);
+  free(memo);
  }
 
-bool kernal_thread(int pid)//kenale thread has cmdline  empty 
+// determin if the process is a theread of the kernel
+// when the process has cmdline empty then it is kernel's thread
+bool kernal_thread(int pid)
 {
- char filename[1000];
-   sprintf(filename, "/proc/%d/cmdline", pid);
-   FILE* kth = fopen(filename, "r");
-   if(!kth)
-    { perror("fopen");  return 1;}
+  char filename[1000];
+  sprintf(filename, "/proc/%d/cmdline", pid);
+  FILE* kth = fopen(filename, "r");
 
-    int c = fgetc(kth);
-    if (c == EOF)
-    {
-    return true;
-    }
-    else
-    return false;
+  if(!kth)
+  {
+   perror("fopen");
+  }
+  //get when char if it is the end of the file then it is empty
+  int c = fgetc(kth);
+  if (c == EOF)
+  {
+   return true;
+  }
+  else
+  return false;
 
-    fclose(kth);
+  fclose(kth);
 }
 
 
-void main(int argc, char **argv) {
+void main(int argc, char **argv)
+{
 //int pids[4194303]={0};//there is no pid is zero
 int pids;
   printf("======================================================================================================================\n");
   printf("|   PID   |  USER   |   PRI   |   NI   |  VIRT  |   RES   |   SHR   |   S   |   CPU%   |   MEM%   |  TIME+ | COMMAND  \n");
   printf("======================================================================================================================\n");
 
-	#pragma omp parallel
-{
- while(1){
-  DIR* proc = opendir("/proc");
-  struct dirent* ent;
+#pragma omp parallel
+//{
+  while(1)
+  {
+   DIR* proc = opendir("/proc");
+   struct dirent* ent;
 
-  if(proc == NULL) {
-    perror("opendir(/proc)");
-    return 1;
-  }
+   if(proc == NULL)
+   {
+     perror("opendir(/proc)");
+   }
 
- while(ent = readdir(proc))
- {
-  if(!isdigit(*ent->d_name))
+   while(ent = readdir(proc))
+   {
+     if(!isdigit(*ent->d_name))
      continue;
-  pids = strtol(ent->d_name, NULL, 10);
-  if(!kernal_thread(pids))
-  //call the calculation function
- process_value(pids);
+     pids = strtol(ent->d_name, NULL, 10);
+     if(!kernal_thread(pids))
+     //call the calculation function
+     process_value(pids);
 
- }
+    }
     update();
     //sleep(1);
     gotoxy(0, 0);
-    usleep(100); 
+    usleep(100);
 
-  closedir(proc);
-  printf("======================================================================================================================\n");
-  printf("|   PID   |  USER   |   PRI   |   NI   |  VIRT  |   RES   |   SHR   |   S   |   CPU%   |   MEM%   |  TIME+ | COMMAND  \n");
-  printf("======================================================================================================================\n");
-
-  }
+   closedir(proc);
+   printf("======================================================================================================================\n");
+   printf("|   PID   |  USER   |   PRI   |   NI   |  VIRT  |   RES   |   SHR   |   S   |   CPU%   |   MEM%   |  TIME+ | COMMAND  \n");
+   printf("======================================================================================================================\n");
  }
+//}
 }
